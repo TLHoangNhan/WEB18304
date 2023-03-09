427518fcf017aec43ac1909ff2846c3b4c3996d6
TLHoangNhan <127101107+TLHoangNhan@users.noreply.github.com>
2023-03-09 12:46:52 +0700

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-//Nhap so nguyen a tu ban phim
-let a = prompt("Nhap so nguyen a");
-//Nhap so nguyen b tu ban phim
-let b = prompt("Nhap so nguyen b");
-let c = parseInt(a) + parseInt(b);
-document.write(c);
-console.log(c);
