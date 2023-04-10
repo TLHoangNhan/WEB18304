f4bc5bc0171be9e02d6bcafe7a4795c039bb341c
TL Hoang Nhan <nhantlhpd08029@fpt.edu.vn>
2023-04-10 17:01:52 +0700

    asm

--- ASM/app.js
+++ ASM/app.js
@@ -36,3 +36,22 @@ const email = document.getElementById("email");
 console.log(email);
 email.addEventListener("keyup", checkEmailValid);
 
+function dongho() {
+  var today = new Date();
+  var gio = today.getHours();
+  var phut = today.getMinutes();
+  var giay = today.getSeconds();
+
+  if (gio < 10) {
+    gio = "0" + gio;
+  }
+  if (phut < 10) {
+    phut = "0" + phut;
+  }
+  if (giay < 10) {
+    giay = "0" + giay;
+  }
+  document.getElementById("time").innerHTML = gio + ":" + phut + ":" + giay;
+  setTimeout("dongho()", 1000);
+}
+dongho();
